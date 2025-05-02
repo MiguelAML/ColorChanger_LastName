f74428f0fa7c1b8310f83407c34d2be5cb0e4a6e
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-05-02 12:43:23 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,3 +1,9 @@
 let button = document.querySelector("colorButton");
 let box = document.querySelector("colorBox");
 
+button.addEventListener("click", function() {
+  // code to change the color goes here
+  box.style.backgroundColor = "blue";
+});
+
+
